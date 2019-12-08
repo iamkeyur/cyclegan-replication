c090adebbb7b9f81e44582b82a9dfac7bc79da74
Keyur <iamkeyur96@gmail.com>
2019-12-08 18:35:07 -0500

    Update download.sh

--- datasets/download.sh
+++ datasets/download.sh
@@ -19,3 +19,7 @@ wget -N $URL -O $ZIP_FILE
 mkdir $TARGET_DIR
 unzip $ZIP_FILE -d ./datasets/
 rm $ZIP_FILE
+mv "$TARGET_DIR/trainA" "$TARGET_DIR/train/A"
+mv "$TARGET_DIR/trainB" "$TARGET_DIR/train/B"
+mv "$TARGET_DIR/testA" "$TARGET_DIR/test/A"
+mv "$TARGET_DIR/testB" "$TARGET_DIR/test/B"
