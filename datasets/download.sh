69e4e5c5cfdc388c3141697ab5aaca910b9765d7
Keyur <iamkeyur96@gmail.com>
2019-12-08 18:37:30 -0500

    Update download.sh

--- datasets/download.sh
+++ datasets/download.sh
@@ -14,7 +14,7 @@ fi
 echo "Specified [$FILE]"
 URL=https://people.eecs.berkeley.edu/~taesung_park/CycleGAN/datasets/$FILE.zip
 ZIP_FILE=./datasets/$FILE.zip
-TARGET_DIR=./datasets/$FILE/
+TARGET_DIR=./datasets/$FILE
 wget -N $URL -O $ZIP_FILE
 mkdir $TARGET_DIR
 unzip $ZIP_FILE -d ./datasets/
